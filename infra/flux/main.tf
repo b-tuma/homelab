221ca5e8712570aeefae96628216c3646ce10128
Bruno Tuma <bruno.tuma@outlook.com>
2021-12-22 18:47:36 -0500

    allow pushing files to github

--- infra/flux/main.tf
+++ infra/flux/main.tf
@@ -92,23 +92,23 @@ resource "github_repository_deploy_key" "main" {
   read_only  = true
 }
 
-# resource "github_repository_file" "install" {
-#   repository = data.github_repository.main.name
-#   file       = data.flux_install.main.path
-#   content    = data.flux_install.main.content
-#   branch     = var.branch
-# }
+resource "github_repository_file" "install" {
+  repository = data.github_repository.main.name
+  file       = data.flux_install.main.path
+  content    = data.flux_install.main.content
+  branch     = var.branch
+}
 
-# resource "github_repository_file" "sync" {
-#   repository = data.github_repository.main.name
-#   file       = data.flux_sync.main.path
-#   content    = data.flux_sync.main.content
-#   branch     = var.branch
-# }
+resource "github_repository_file" "sync" {
+  repository = data.github_repository.main.name
+  file       = data.flux_sync.main.path
+  content    = data.flux_sync.main.content
+  branch     = var.branch
+}
 
-# resource "github_repository_file" "kustomize" {
-#   repository = data.github_repository.main.name
-#   file       = data.flux_sync.main.kustomize_path
-#   content    = data.flux_sync.main.kustomize_content
-#   branch     = var.branch
-# }
+resource "github_repository_file" "kustomize" {
+  repository = data.github_repository.main.name
+  file       = data.flux_sync.main.kustomize_path
+  content    = data.flux_sync.main.kustomize_content
+  branch     = var.branch
+}
